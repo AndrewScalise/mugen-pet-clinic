a5e092c9fe3eafd53c2f0bd6281bd98e4a651955
Mugen <paradigmcomplex@gmail.com>
2020-01-15 21:02:41 -0600

    scaffolding for pet clinic

---
+++ src/main/java/mugen/fulldev/mugenpetclinic/MugenPetClinicApplication.java
@@ -0,0 +1,13 @@
+package mugen.fulldev.mugenpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugenPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MugenPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mugen/fulldev/mugenpetclinic/MugenPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mugen.fulldev.mugenpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugenPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
